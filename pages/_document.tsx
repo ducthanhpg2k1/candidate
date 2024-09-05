4d5f810f9e12bfdf8488e59058524fdabb21852d
thanhld <thanhld@peasoft.vn>
2024-09-05 15:45:25 +0700

    update font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,7 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap'
           rel='stylesheet'
         />
+
         <link
           href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap'
           rel='stylesheet'
@@ -21,6 +22,10 @@ export default function Document() {
         ></link>
         <link
           href='https://fonts.cdnfonts.com/css/segoe-ui-4?styles=18006,18005,18004,18003'
+          rel='stylesheet'
+        ></link>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap'
           rel='stylesheet'
         ></link>
       </Head>
